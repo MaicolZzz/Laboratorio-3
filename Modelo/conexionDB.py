6d2790ae4664bed8097c4f4b104d6ccfd2521e1e
MaicolZzz <buitragomaicol23@gmail.com>
2024-11-23 10:50:43 -0500

    Estrutura mvc

---
+++ Modelo/conexionDB.py
@@ -0,0 +1,29 @@
+import mariadb as sql
+
+
+class conexion():
+    def __init__(self):
+        self.__host ="localhost"
+        self.__user="root"
+        self.__password =""
+        self.__port = 3307
+        self.__database="mvc"
+        self.__conection = None
+
+    def crearConexion(self):
+        self.__conection= sql.connect(
+            host= self.__host,
+            user = self.__user,
+            password = self.__password,
+            port = self.__port,
+            database=self.__database
+        )
+   
+    def cerrarConexion(self):
+        if self.__conection:
+            self.__conection.close()
+            self.__conection= None
+    
+    #poner aqui los getters y settes
+    def getConection(self):
+        return self.__conection
---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+class Main():
+    def main():
+        app = None
+    main()
